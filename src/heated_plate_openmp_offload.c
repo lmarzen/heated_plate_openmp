3e7c123938dd853c912e8e5dd221d061311323ed
Luke Marzen <marzenlj@gmail.com>
2022-10-09 15:56:35 -0500

    fixed formatting

--- src/heated_plate_openmp_offload.c
+++ src/heated_plate_openmp_offload.c
@@ -3,108 +3,106 @@
 #include <math.h>
 #include <omp.h>
 
-int main(int argc, char *argv[]);
-
-/******************************************************************************/
-
+/* Purpose:
+ *
+ *   MAIN is the main program for HEATED_PLATE_OPENMP.
+ *
+ * Discussion:
+ *
+ *   This code solves the steady state heat equation on a rectangular region.
+ *
+ *   The sequential version of this program needs approximately
+ *   18/epsilon iterations to complete.
+ *
+ *
+ *   The physical region, and the boundary conditions, are suggested
+ *   by this diagram;
+ *
+ *                  W = 0
+ *            +------------------+
+ *            |                  |
+ *   W = 100  |                  | W = 100
+ *            |                  |
+ *            +------------------+
+ *                  W = 100
+ *
+ *   The region is covered with a grid of M by N nodes, and an N by N
+ *   array W is used to record the temperature.  The correspondence between
+ *   array indices and locations in the region is suggested by giving the
+ *   indices of the four corners:
+ *
+ *                 I = 0
+ *         [0][0]-------------[0][N-1]
+ *            |                  |
+ *     J = 0  |                  |  J = N-1
+ *            |                  |
+ *       [M-1][0]-----------[M-1][N-1]
+ *                 I = M-1
+ *
+ *   The steady state solution to the discrete heat equation satisfies the
+ *   following condition at an interior grid point:
+ *
+ *     W[Central] = (1/4) * ( W[North] + W[South] + W[East] + W[West] )
+ *
+ *   where "Central" is the index of the grid point, "North" is the index
+ *   of its immediate neighbor to the "north", and so on.
+ *
+ *   Given an approximate solution of the steady state heat equation, a
+ *   "better" solution is given by replacing each interior point by the
+ *   average of its 4 neighbors - in other words, by using the condition
+ *   as an ASSIGNMENT statement:
+ *
+ *     W[Central]  <=  (1/4) * ( W[North] + W[South] + W[East] + W[West] )
+ *
+ *   If this process is repeated often enough, the difference between successive
+ *   estimates of the solution will go to zero.
+ *
+ *   This program carries out such an iteration, using a tolerance specified by
+ *   the user, and writes the final estimate of the solution to a file that can
+ *   be used for graphic processing.
+ *
+ * Licensing:
+ *
+ *   This code is distributed under the GNU LGPL license.
+ *
+ * Modified:
+ *
+ *   09 October 2022
+ *
+ * Author:
+ *
+ *   Original C version by Michael Quinn.
+ *   Modified version by John Burkardt.
+ *   Modified by Luke Marzen to add OpenMP GPU offload support.
+ *
+ * Reference:
+ *
+ *   Michael Quinn,
+ *   Parallel Programming in C with MPI and OpenMP,
+ *   McGraw-Hill, 2004,
+ *   ISBN13: 978-0071232654,
+ *   LC: QA76.73.C15.Q55.
+ *
+ * Local parameters:
+ *
+ *   Local, double DIFF, the norm of the change in the solution from one iteration
+ *   to the next.
+ *
+ *   Local, double MEAN, the average of the boundary values, used to initialize
+ *   the values of the solution in the interior.
+ *
+ *   Local, double U[M][N], the solution at the previous iteration.
+ *
+ *   Local, double W[M][N], the solution computed at the latest iteration.
+ */
 int main(int argc, char *argv[])
-
-/******************************************************************************/
-/*
-  Purpose:
-
-    MAIN is the main program for HEATED_PLATE_OPENMP.
-
-  Discussion:
-
-    This code solves the steady state heat equation on a rectangular region.
-
-    The sequential version of this program needs approximately
-    18/epsilon iterations to complete.
-
-
-    The physical region, and the boundary conditions, are suggested
-    by this diagram;
-
-                   W = 0
-             +------------------+
-             |                  |
-    W = 100  |                  | W = 100
-             |                  |
-             +------------------+
-                   W = 100
-
-    The region is covered with a grid of M by N nodes, and an N by N
-    array W is used to record the temperature.  The correspondence between
-    array indices and locations in the region is suggested by giving the
-    indices of the four corners:
-
-                  I = 0
-          [0][0]-------------[0][N-1]
-             |                  |
-      J = 0  |                  |  J = N-1
-             |                  |
-        [M-1][0]-----------[M-1][N-1]
-                  I = M-1
-
-    The steady state solution to the discrete heat equation satisfies the
-    following condition at an interior grid point:
-
-      W[Central] = (1/4) * ( W[North] + W[South] + W[East] + W[West] )
-
-    where "Central" is the index of the grid point, "North" is the index
-    of its immediate neighbor to the "north", and so on.
-
-    Given an approximate solution of the steady state heat equation, a
-    "better" solution is given by replacing each interior point by the
-    average of its 4 neighbors - in other words, by using the condition
-    as an ASSIGNMENT statement:
-
-      W[Central]  <=  (1/4) * ( W[North] + W[South] + W[East] + W[West] )
-
-    If this process is repeated often enough, the difference between successive
-    estimates of the solution will go to zero.
-
-    This program carries out such an iteration, using a tolerance specified by
-    the user, and writes the final estimate of the solution to a file that can
-    be used for graphic processing.
-
-  Licensing:
-
-    This code is distributed under the GNU LGPL license.
-
-  Modified:
-
-    18 October 2011
-
-  Author:
-
-    Original C version by Michael Quinn.
-    This C version by John Burkardt.
-
-  Reference:
-
-    Michael Quinn,
-    Parallel Programming in C with MPI and OpenMP,
-    McGraw-Hill, 2004,
-    ISBN13: 978-0071232654,
-    LC: QA76.73.C15.Q55.
-
-  Local parameters:
-
-    Local, double DIFF, the norm of the change in the solution from one iteration
-    to the next.
-
-    Local, double MEAN, the average of the boundary values, used to initialize
-    the values of the solution in the interior.
-
-    Local, double U[M][N], the solution at the previous iteration.
-
-    Local, double W[M][N], the solution computed at the latest iteration.
-*/
 {
-#define M 1000
-#define N 1000
+#ifndef M
+#define M 500
+#endif
+#ifndef N
+#define N 500
+#endif
 
   double diff;
   double epsilon = 0.001;
@@ -125,12 +123,11 @@ int main(int argc, char *argv[])
   printf("  The iteration will be repeated until the change is <= %e\n", epsilon);
   printf("  Number of processors available = %d\n", omp_get_num_procs());
   printf("  Number of threads =              %d\n", omp_get_max_threads());
-  /*
-    Set the boundary values, which don't change.
-  */
+
+  // Set the boundary values, which don't change.
   mean = 0.0;
 
-#pragma omp target data map(to:u,w)
+#pragma omp target data map(alloc:u, w)
   {
 #pragma omp target teams distribute parallel for
     for (int i = 1; i < M - 1; i++)
@@ -144,10 +141,9 @@ int main(int argc, char *argv[])
       w[M - 1][j] = 100.0;
       w[0][j] = 0.0;
     }
-    /*
-      Average the boundary values, to come up with a reasonable
-      initial value for the interior.
-    */
+    
+    // Average the boundary values, to come up with a reasonable initial value
+    // for the interior.
 #pragma omp target data map(tofrom:mean)
     {
 #pragma omp target teams distribute parallel for reduction(+:mean)
@@ -162,19 +158,15 @@ int main(int argc, char *argv[])
       }
     } // end #pragma omp target data map(tofrom:mean)
 
-    /*
-      OpenMP note:
-      You cannot normalize MEAN inside the parallel region.  It
-      only gets its correct value once you leave the parallel region.
-      So we interrupt the parallel region, set MEAN, and go back in.
-    */
+    // OpenMP note:
+    // You cannot normalize MEAN inside the parallel region. It only gets its
+    // correct value once you leave the parallel region. So we interrupt the
+    // parallel region, set MEAN, and go back in.
     mean = mean / (double)(2 * M + 2 * N - 4);
     printf("\n");
     printf("  MEAN = %f\n", mean);
-    /*
-      Initialize the interior solution to the mean value.
-    */
 
+    // Initialize the interior solution to the mean value.
 #pragma omp target teams distribute parallel for map(to:mean) collapse(2)
     for (int i = 1; i < M - 1; i++)
     {
@@ -183,10 +175,9 @@ int main(int argc, char *argv[])
         w[i][j] = mean;
       }
     }
-    /*
-      iterate until the  new solution W differs from the old solution U
-      by no more than EPSILON.
-    */
+     
+    // iterate until the  new solution W differs from the old solution U by no
+    // more than EPSILON.
     iterations = 0;
     iterations_print = 1;
     printf("\n");
@@ -195,13 +186,10 @@ int main(int argc, char *argv[])
     wtime = omp_get_wtime();
 
     diff = epsilon;
-// #pragma omp target data map(tofrom:diff,iterations)
-//       {
+
     while (epsilon <= diff)
     {
-/*
-  Save the old solution in U.
-*/
+      // Save the old solution in U.
 #pragma omp target teams distribute parallel for collapse(2)
       for (int i = 0; i < M; i++)
       {
@@ -210,10 +198,9 @@ int main(int argc, char *argv[])
           u[i][j] = w[i][j];
         }
       }
-/*
-  Determine the new estimate of the solution at the interior points.
-  The new solution W is the average of north, south, east and west neighbors.
-*/
+      
+      // Determine the new estimate of the solution at the interior points. The
+      // new solution W is the average of north, south, east and west neighbors.
 #pragma omp target teams distribute parallel for collapse(2)
       for (int i = 1; i < M - 1; i++)
       {
@@ -222,43 +209,33 @@ int main(int argc, char *argv[])
           w[i][j] = (u[i - 1][j] + u[i + 1][j] + u[i][j - 1] + u[i][j + 1]) / 4.0;
         }
       }
-      /*
-        C and C++ cannot compute a maximum as a reduction operation.
 
-        Therefore, we define a private variable MY_DIFF for each thread.
-        Once they have all computed their values, we use a CRITICAL section
-        to update DIFF.
-      */
       diff = 0.0;
-      // #pragma omp target update to(diff)
-// #pragma omp target data map(tofrom:diff)
-//       {
-  // #pragma omp target
-  // {
 #pragma omp target teams distribute parallel for reduction(max:diff) map(tofrom:diff)
-        for (int i = 1; i < M - 1; i++)
+      for (int i = 1; i < M - 1; i++)
+      {
+        double my_diff = 0.0;
+        for (int j = 1; j < N - 1; j++)
         {
-          double my_diff = 0.0;
-          for (int j = 1; j < N - 1; j++)
-          {
-            my_diff = fmax(my_diff, fabs(w[i][j] - u[i][j]));
-          }
-          diff = fmax(diff, my_diff);
+          my_diff = fmax(my_diff, fabs(w[i][j] - u[i][j]));
         }
-//}
-      // } // end #pragma omp target data map(tofrom:diff)
-  // #pragma omp target update from(diff)
+        diff = fmax(diff, my_diff);
+      }
       iterations++;
       if (iterations == iterations_print)
       {
         printf("  %8d  %f\n", iterations, diff);
         iterations_print = 2 * iterations_print;
       }
-    } // end while
-        // } // end #pragma omp target
-      // } // end #pragma omp target data map(tofrom:diff)
-  } // end #pragma omp target data map(to:u,w)
+    } // end while-loop
 
+// Only copy array w from device to host, results are stored in w. Array u is
+// used to store intermediate values so there would never be a practical
+// reason to save u.
+#pragma omp target update from(w)
+  } // end #pragma omp target data map(alloc:u, w)
+
+  wtime = omp_get_wtime() - wtime;
 
   printf("\n");
   printf("  %8d  %f\n", iterations, diff);
