e961ac28d4f1e341e21f3cd3c218fb43fc17e413
Luke Marzen <marzenlj@gmail.com>
2022-10-09 18:29:22 -0500

    updated script

--- benchmark.sh
+++ benchmark.sh
@@ -3,12 +3,12 @@
 cd src
 make clean > /dev/null 2>&1
 
-for SZ in 100 500 1000 5000
+for SZ in 100 500 1000 5000 10000
 do
   echo "${SZ}x${SZ} Matrix--------"
   DIM="-DM=$SZ -DN=$SZ" make > /dev/null 2>&1
-  cal_t=$((time OMP_NUM_THREADS=1 ./heated_plate_cpu)  2>&1 > /dev/null | grep real | awk '{print $2}')
-  echo "sequential on cpu   $cal_t"
+  # cal_t=$((time OMP_NUM_THREADS=1 ./heated_plate_cpu)  2>&1 > /dev/null | grep real | awk '{print $2}')
+  # echo "sequential on cpu   $cal_t"
   cal_t=$((time ./heated_plate_cpu)  2>&1 > /dev/null | grep real | awk '{print $2}')
   echo "parallel on cpu     $cal_t"
   cal_t=$((time ./heated_plate_gpu)  2>&1 > /dev/null | grep real | awk '{print $2}')
