c5a84224923a276e69be79ede1e067c4fb976b7e
Luke Marzen <marzenlj@gmail.com>
2022-10-23 19:49:35 -0500

    updated benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -3,18 +3,27 @@
 cd src
 make clean > /dev/null 2>&1
 
-for SZ in 100 500 1000 5000 10000
+for SZ in 100 500 1000
 do
-  echo "${SZ}x${SZ} Matrix--------"
+  echo "-----------------------------------${SZ}x${SZ} Matrix"
   DIM="-DM=$SZ -DN=$SZ" make > /dev/null 2>&1
-  # cal_t=$((time OMP_NUM_THREADS=1 ./heated_plate_cpu)  2>&1 > /dev/null | grep real | awk '{print $2}')
-  # echo "sequential on cpu   $cal_t"
-  cal_t=$((time ./heated_plate_cpu)  2>&1 > /dev/null | grep real | awk '{print $2}')
-  echo "parallel on cpu     $cal_t"
-  cal_t=$((time ./heated_plate_gpu)  2>&1 > /dev/null | grep real | awk '{print $2}')
-  echo "parallel on gpu     $cal_t"
+  echo "sequential on cpu:"
+  ./heated_plate_sequential -qt
+  echo "parallel (openmp) on cpu:"
+  ./heated_plate_openmp_cpu -qt
+  echo "parallel (openmp) offloaded to gpu:"
+  ./heated_plate_openmp_offload -qt
   make clean > /dev/null 2>&1
 done
+
+SZ=5000
+echo "-----------------------------------${SZ}x${SZ} Matrix"
+DIM="-DM=$SZ -DN=$SZ" make > /dev/null 2>&1
+echo "parallel (openmp) on cpu:"
+./heated_plate_openmp_cpu -qt
+echo "parallel (openmp) offloaded to gpu:"
+./heated_plate_openmp_offload -qt
+make clean > /dev/null 2>&1
 
 cd ../
 echo "Done."
